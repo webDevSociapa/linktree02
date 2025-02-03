820b43e4acff27a7777d1494b0896d8326e17279
webDevSociapa <webdev@sociapa.com>
2025-02-03 19:34:54 +0530

    eff

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -4,29 +4,38 @@ import CartSummary from "../../components/CartSummary"
 import ShippingForm from "../../components/ShippingForm"
 import PaymentForm from "../../components/PaymentForm"
 import Confirmation from "../../components/Confirmation"
+import { useSearchParams } from "next/navigation"
 
 // Mock cart data
-const cart = [
-  {
-    id: 1,
-    name: "Basic Tee",
-    href: "#",
-    price: 32.0,
-    color: "Sienna",
-    size: "Large",
-    imageSrc: "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-01.jpg",
-    imageAlt: "Front of men's Basic Tee in sienna.",
-  },
-  // Add more items as needed
-]
 
-export default function Checkout({name}) {
 
-    console.log("name",name);
+export default function Checkout() {
+
+    const searchParams = useSearchParams();
+
+    const search = searchParams.get("name");
+    console.log("name",search);
+    
+
     
   const [step, setStep] = useState(0)
 
   const nextStep = () => setStep(step + 1)
+
+
+  const cart = [
+    {
+      id: 1,
+      name:search,
+      href: "#",
+      price: 32.0,
+      color: "Sienna",
+      size: "Large",
+      imageSrc: "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-01.jpg",
+      imageAlt: "Front of men's Basic Tee in sienna.",
+    },
+    // Add more items as needed
+  ]
 
   return (
     <CheckoutLayout currentStep={step}>
