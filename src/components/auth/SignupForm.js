138422c85827194440a09437dffa74bc51831733
webDevSociapa <webdev@sociapa.com>
2025-02-14 12:52:22 +0530

    ghghgh

--- src/components/auth/SignupForm.js
+++ src/components/auth/SignupForm.js
@@ -7,7 +7,7 @@ import axios from "axios";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useDispatch } from "react-redux";
-import LoadingSpinner from "@/components/LoadingSpinner";
+import LoadingSpinner from "../../components/LoadingSpinner";
 import { signUpRequest } from "@/redux/slices/authSlice";
 
 export default function SignupForm() {
