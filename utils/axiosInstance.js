e9b77dc957774f986b5076f7c0d3d16ad02cf123
webDevSociapa <webdev@sociapa.com>
2025-02-14 17:49:52 +0530

    wwwwww

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000",
-  timeout: 10000, // Set timeout (10 seconds)
   
   headers: {
     "Content-Type": "application/json",
