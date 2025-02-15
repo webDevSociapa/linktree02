196988f8a4ab1568b1031025a11b0e4e1bbcd595
webDevSociapa <webdev@sociapa.com>
2025-02-15 13:39:41 +0530

    Update axiosInstance.js

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://localhost:3000",
   
   headers: {
     "Content-Type": "application/json",
