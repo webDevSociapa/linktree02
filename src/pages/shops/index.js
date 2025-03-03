b9d72ad3b09253272da44de614d70bca26e5baa1
webDevSociapa <webdev@sociapa.com>
2025-03-03 13:30:46 +0530

    fff

---
+++ src/pages/shops/index.js
@@ -0,0 +1,10 @@
+import PagesList from "@/components/common/pagesList";
+
+export default function Shops() {
+    return (
+       <>
+       <PagesList/>
+       <h2 className="text-center border-2">Loading ...</h2>
+       </> 
+    )
+}
