65747e73bbe89be9609ea516897ef6eeb509f249
webDevSociapa <webdev@sociapa.com>
2025-04-26 12:42:42 +0530

    fixess

--- src/pages/appearance/index.js
+++ src/pages/appearance/index.js
@@ -39,7 +39,7 @@ export default function Appearance() {
     }
   }
 
-  const handleUpdateProfileImage = async (e) => {
+  const   handleUpdateProfileImage = async (e) => {
     e.preventDefault()
     try {
       const data = new FormData()
