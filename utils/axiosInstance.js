4cdeb41377f2f8a66102b0452722cf0bd6c73d41
webDevSociapa <webdev@sociapa.com>
2025-05-02 13:09:05 +0530

    cvvc

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://www.followus.link",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://linktreenew.vercel.app",
   
   headers: {
     "Content-Type": "application/json",
