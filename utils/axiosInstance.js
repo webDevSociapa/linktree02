317d611cfd710fdfae3c13df401078ce1a571f12
webDevSociapa <webdev@sociapa.com>
2025-05-13 13:18:01 +0530

    Update axiosInstance.js

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -2,13 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://followus.link",
-  
   headers: {
-    "Content-Type": "application/json",
     'Content-Type': 'multipart/form-data',
-    
-    "Access-Control-Allow-Origin": "https://followus.link", // Allow all origins (for dev only)
-
   },
 });
 
